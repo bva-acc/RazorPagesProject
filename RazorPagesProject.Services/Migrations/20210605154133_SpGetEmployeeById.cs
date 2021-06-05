22e89a258880c868c21f0d6151c15961f4ba4dd4
Вадим Булат <justliveinlife@gmail.com>
2021-06-05 19:32:03 +0300

    feat: add migrations for stored procedures

---
+++ RazorPagesProject.Services/Migrations/20210605154133_SpGetEmployeeById.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesProject.Services.Migrations
+{
+    public partial class SpGetEmployeeById : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Create procedure spGetEmployeeById
+                                @Id int
+                                as
+                                Begin
+	                                Select * from Employees
+	                                Where Id = @Id
+                                End";
+            migrationBuilder.Sql(procedure);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            string procedure = @"Drop procedure spGetEmployeeById";
+            migrationBuilder.Sql(procedure);
+        }
+    }
+}
